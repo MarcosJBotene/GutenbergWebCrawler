64b23adbb65a5f9a6cbbfb49eed2d6385faf0d50
Marcos Botene <marcos_j_martins@estudante.sc.senai.br>
2020-11-11 20:59:56 -0200

    Added POO.

---
+++ util/GutenbergCrawler.php
@@ -0,0 +1,94 @@
+<?php
+
+class GutenbergCrawler {
+
+    private $url;
+    private $proxy;
+    private $dom;
+    private $html;
+
+    public function __construct() {
+        $this->url = "http://gutenberg.org";
+        $this->proxy = "10.1.21.254:3128";
+        $this->dom = new DOMDocument();
+    }
+
+    public function getParagraphs() {
+        $this->loadHTML();
+        $divTags = $this->captureDivTags();
+        $internalDiv = $this->captureInternalDivsPageContent($divTags);
+        $pTags = $this->capturePTags($internalDiv);
+        $arrayParagraphs = $this->getArrayParagraphs($pTags);
+        return $arrayParagraphs;
+    }
+
+    private function getContextConnection() {
+        $arrayConfig = array(
+            'http' => array(
+                'proxy' => $this->proxy,
+                'request_fulluri' => true
+            ),
+            'https' => array(
+                'proxy' => $this->proxy,
+                'request_fulluri' => true
+            )
+        );
+
+        $context = stream_context_create($arrayConfig);
+        return $context;
+    }
+
+    private function loadHTML() {
+        $context = $this->getContextConnection();
+        $this->html = file_get_contents($this->url, false, $context);
+
+        libxml_use_internal_errors(true);
+
+        $this->dom->loadHTML($this->html);
+        libxml_clear_errors();
+    }
+
+    private function captureDivTags() {
+        $divTags = $this->dom->getElementsByTagName('div');
+        return $divTags;
+    }
+
+    private function captureInternalDivsPageContent($divsGeral) {
+        $internalDivs = null;
+
+        foreach ($divsGeral as $div) {
+            $class = $div->getAttribute('class');
+
+            if ($class == 'page_content') {
+                $internalDivs = $div->getElementsByTagName('div');
+                break;
+            }
+        }
+
+        return $internalDivs;
+    }
+
+    private function capturePTags($internalDivs) {
+        $pTags = null;
+
+        foreach ($internalDivs as $internalDiv) {
+            $internalClass = $internalDiv->getAttribute('class');
+            if ($internalClass == 'box_announce') {
+                $pTags = $internalDiv->getElementsByTagName('p');
+            }
+        }
+
+        return $pTags;
+    }
+
+    private function getArrayParagraphs($pTags) {
+        $arrayParagraphs = [];
+
+        foreach ($pTags as $p) {
+            $arrayParagraphs[] = $p->nodeValue;
+        }
+
+        return $arrayParagraphs;
+    }
+
+}
